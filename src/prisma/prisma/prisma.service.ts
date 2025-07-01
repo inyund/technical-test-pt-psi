77631da1b4c531ba198ddd8411194da04fbbc190
Muhammad Farouq Rafie <mfrafie@gmail.com>
2025-07-01 18:56:52 +0700

    feat(prisma): add global PrismaService module

---
+++ src/prisma/prisma/prisma.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from 'generated/prisma';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super({
+      log: ['query', 'info', 'warn', 'error'],
+    });
+  }
+}
