287fd6bc12589876d907e52017bc22d5e830efa9
Muhammad Farouq Rafie <mfrafie@gmail.com>
2025-07-01 18:01:20 +0700

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT');
+  const cookieSecretKey: string = configService.get('COOKIE_SECRET_KEY') || '';
+  if (!cookieSecretKey) {
+    throw new Error('COOKIE_SECRET_KEY is not defined');
+  } else {
+    app.use(cookieParser(cookieSecretKey));
+  }
+
+  await app.listen(port || 8000);
+  return port;
+}
+bootstrap()
+  .then((port) => console.log('Server started. listening on port', port))
+  .catch(console.error);
