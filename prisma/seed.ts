0f85409bfddf9e7b3fca6859f42e54510243e2e4
Muhammad Farouq Rafie <mfrafie@gmail.com>
2025-07-02 11:19:18 +0700

    feat(prisma): add User and Company models with seed

---
+++ prisma/seed.ts
@@ -0,0 +1,55 @@
+import { PrismaClient } from '../generated/prisma';
+const prisma = new PrismaClient();
+async function main() {
+  await prisma.user.deleteMany();
+  await prisma.company.deleteMany();
+
+  const users = [
+    {
+      name: 'Imron',
+      phone: '081234567890',
+    },
+    {
+      name: 'Juli',
+      email: 'Sammny@mail.com',
+      phone: '0987654321',
+    },
+    {
+      name: 'Gajah Mada',
+    },
+  ];
+
+  const [imron, juli] = await Promise.all(
+    users.map((user) =>
+      prisma.user.create({
+        data: user,
+      }),
+    ),
+  );
+
+  await prisma.company.createMany({
+    data: [
+      {
+        user_id: imron.id,
+        company_code: 'SPI',
+      },
+      {
+        company_name: 'Samudera',
+      },
+      {
+        user_id: juli.id,
+        company_code: 'PIC',
+        company_name: 'Samudera',
+      },
+    ],
+  });
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
