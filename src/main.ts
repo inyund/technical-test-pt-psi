5ebec05e6211d371ca6d0fb8a119815d1d22cbeb
Muhammad Farouq Rafie <mfrafie@gmail.com>
2025-07-02 08:48:55 +0700

    feat(main): add Swagger API documentation setup

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,40 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: ['log', 'error', 'warn', 'debug', 'verbose'],
+  });
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT');
+
   const cookieSecretKey: string = configService.get('COOKIE_SECRET_KEY') || '';
+
   if (!cookieSecretKey) {
     throw new Error('COOKIE_SECRET_KEY is not defined');
   } else {
     app.use(cookieParser(cookieSecretKey));
   }
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('NestJS Technical Test PT PSI')
+    .setDescription('NestJS API documentation for Technical Test PT PSI')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api-docs', app, documentFactory);
+
   app.setGlobalPrefix('api');
+
   await app.listen(port || 8000);
+
   return port;
 }
 bootstrap()
-  .then((port) => console.log('Server started. listening on port', port))
+  .then((port) => {
+    console.log('Server started. listening on port', port);
+  })
   .catch(console.error);
